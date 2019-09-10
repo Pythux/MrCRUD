e437b16f387df192ffca44ca22b699058ce30181
Pythux <pythux@nowhere.org>
2019-09-10 10:14:04 +0200

    create DjangoTemplate1, add media urls and helloworld

---
+++ DjangoTemplate1/myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    content = models.TextField()
+
+    def __str__(self):
+        self.title
