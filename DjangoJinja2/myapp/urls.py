2d7ce68395075280e016622f099659395670a3b5
Pythux <pythux@nowhere.org>
2019-09-12 14:25:31 +0200

    begin CBV -> Class Based Views

--- DjangoJinja2/myapp/urls.py
+++ DjangoJinja2/myapp/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'myapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- DjangoJinja2/myapp/views.py
+++ DjangoJinja2/myapp/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'hello.jinja')
+class IndexView(TemplateView):
+    template_name = 'hello.jinja'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['inject'] = "yolo"
+        return context
