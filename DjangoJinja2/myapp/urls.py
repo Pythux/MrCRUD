695d9cf5be1bb85260893394f1d8022476d300fa
Pythux <pythux@nowhere.org>
2019-09-12 18:18:47 +0200

    post_detail, _list

--- DjangoJinja2/myapp/urls.py
+++ DjangoJinja2/myapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'myapp'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('hello', views.IndexView.as_view(), name='index'),
     path('detail/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
-    path('list', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
 ]
