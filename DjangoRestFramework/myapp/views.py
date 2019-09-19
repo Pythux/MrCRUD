90729eb9df3bb05f655f5502d9cfc6b6c9d2d209
Pythux <pythux@nowhere.org>
2019-09-19 11:12:50 +0200

    Using mixins

--- DjangoRestFramework/myapp/views.py
+++ DjangoRestFramework/myapp/views.py
@@ -1,54 +1,40 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from rest_framework.views import APIView
-from rest_framework import status
-from . import models
+# from django.http import Http404
+# from django.shortcuts import get_object_or_404
+#
+# from rest_framework.response import Response
+# from rest_framework.parsers import JSONParser
+from rest_framework import mixins, generics
+
+from .models import Country
 from .serializers import CountrySerializer
 
 
-class CountryList(APIView):
-    queryset = models.Country.objects.all()
+class CountryList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
 
-    def get(self, request, format=None):
-        country = models.Country.objects.all()
-        serializer = CountrySerializer(country, many=True)
-        return Response(serializer.data)
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
 
-    def post(self, request, format=None):
-        serializer = CountrySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class CountryDetail(APIView):
-    queryset = models.Country.objects.all()
+class CountryDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
 
-    def get_object(self, pk):
-        try:
-            return models.Country.objects.get(pk=pk)
-        except models.Country.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        """Retrieve, update or delete a Country."""
-        country = self.get_object(pk)
-        serializer = CountrySerializer(country)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        country = get_object_or_404(models.Country, pk=pk)
-        data = JSONParser().parse(request)
-        serializer = CountrySerializer(country, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        country = get_object_or_404(models.Country, pk=pk)
-        country.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
