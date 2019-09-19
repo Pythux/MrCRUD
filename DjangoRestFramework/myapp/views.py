af7ee48a794f8fb88243a484f8a32d1c521efead
Pythux <pythux@nowhere.org>
2019-09-19 11:53:18 +0200

    generic Class-based Views

--- DjangoRestFramework/myapp/views.py
+++ DjangoRestFramework/myapp/views.py
@@ -3,38 +3,17 @@
 #
 # from rest_framework.response import Response
 # from rest_framework.parsers import JSONParser
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from .models import Country
 from .serializers import CountrySerializer
 
 
-class CountryList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-
+class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class CountryDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class CountryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
