6046a3b0804dc9a7c5776fba89ba77f89564d4ae
Pythux <pythux@nowhere.org>
2019-10-07 13:20:18 +0200

    rename MVC

--- DjangoRestFramework/myapp/urls.py
+++ DjangoRestFramework/myapp/urls.py
@@ -1,16 +1,16 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
+from . import controller
 
 
 app_name = 'myapp'
 
 
-# Create a router and register our viewsets with it.
+# Create a router and register our controllers with it.
 router = DefaultRouter(trailing_slash=False)
-router.register(r'user', views.UserViewSet)
-router.register('post', views.PostViewSet)
+router.register(r'user', controller.UserViewSet)
+router.register('post', controller.PostViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
